a4e3d1741832b13872fbe70e7ebb6851278d6ae5
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.pl>
2023-03-08 20:58:22 +0100

    Initial commit

---
+++ src/main/java/com/example/unittests/task2/CitiesSearch.java
@@ -0,0 +1,14 @@
+package com.example.unittests.task2;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+class CitiesSearch {
+
+    Set<String> search(String inputText) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+}
